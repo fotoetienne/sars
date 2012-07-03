2a241428e3efdac06586a73e506d16613fc60554
stephen <root@lmsnhost>
2012-07-02 21:38:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sars::Application.config.secret_token = 'f515664995dbff1b2bf1248dadc25cbcaa4c5e3ca9dea51af60c715c608b3b741aabbb978c884dd9cf57a5957ce7506897c6bc8fe7e0882a9956452b7bd47888'
