ef83e9699a4b12eb1ae892a035dc96d7142ed637
stephen <root@lmsnhost>
2012-07-10 12:08:53 -0400

    }

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -4,7 +4,7 @@ class Rsvp
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_accessor :name, :email, :response, :name2, :response2, :name3, :response3, :name4, :response4, :name5, :response5, :name6, :response6
+  attr_accessor :name, :email, :response, :name2, :response2, :name3, :response3, :name4, :response4, :name5, :response5, :name6, :response6, :comments
 
   validates :name, :response, :presence => true
   #validates :email, :format => { :with => %r{.+@.+\..+} }, :allow_blank => true
